788fda7956e76189c426140d99294f253948384c
natan-mlk <adam.milko@gmail.com>
2021-01-30 18:28:17 +0100

    added featured-app route

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { FeaturedAppWorkspaceComponent } from './featured-app/featured-app-workspace/featured-app-workspace.component';
 import { GalleryWorkspaceComponent } from './gallery/gallery-workspace/gallery-workspace.component';
 import { MainContentComponent } from './main-content/main-content.component';
 import { ProfessionWorkspaceComponent } from './profession/profession-workspace/profession-workspace.component';
@@ -8,7 +9,7 @@ const routes: Routes = [
   {path: '', component: MainContentComponent},
   {path: 'gallery', component: GalleryWorkspaceComponent},
   {path: 'profession', component: ProfessionWorkspaceComponent},
-  // {path: 'featured', component: .....},
+  {path: 'featured', component: FeaturedAppWorkspaceComponent}
 ];
 
 @NgModule({
