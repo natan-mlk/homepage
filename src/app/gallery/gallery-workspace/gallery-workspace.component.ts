dbd019a4b07bfbd583caf1bfdbd0db70f53f4666
natan-mlk <adam.milko@gmail.com>
2021-02-01 20:45:19 +0100

    added Lightbox library and working test version of gallery

--- src/app/gallery/gallery-workspace/gallery-workspace.component.ts
+++ src/app/gallery/gallery-workspace/gallery-workspace.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { IAlbum, Lightbox, LightboxConfig } from 'ngx-lightbox';
 
 @Component({
   selector: 'app-gallery-workspace',
@@ -7,9 +8,38 @@ import { Component, OnInit } from '@angular/core';
 })
 export class GalleryWorkspaceComponent implements OnInit {
 
-  constructor() { }
+  private album: Array<IAlbum> = [];
+
+  constructor(
+    private lightbox: Lightbox,
+    private lightboxConfig: LightboxConfig) {
+    lightboxConfig.centerVertically = true;
+    this.album.push(
+      {
+        src: 'http://placeimg.com/640/480/nature',
+         caption: 'nature',
+         thumb: 'http://placeimg.com/140/80/nature'
+      },
+      {
+        src: 'http://placeimg.com/640/480/animals',
+         caption: 'animals',
+         thumb: 'http://placeimg.com/140/80/animals'
+      },
+    )
+  }
 
   ngOnInit(): void {
   }
+
+  open(index: number): void {
+    // open lightbox
+    this.lightbox.open(this.album, index);
+  }
+ 
+  close(): void {
+    // close lightbox programmatically
+    this.lightbox.close();
+  }
+
 
 }
--- src/app/gallery/gallery.module.ts
+++ src/app/gallery/gallery.module.ts
@@ -1,13 +1,15 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { GalleryWorkspaceComponent } from './gallery-workspace/gallery-workspace.component';
+import { LightboxModule } from 'ngx-lightbox';
 
 
 
 @NgModule({
   declarations: [GalleryWorkspaceComponent],
   imports: [
-    CommonModule // this is for importing angular stuff np directives like ngFor 
+    CommonModule, // this is for importing angular stuff np directives like ngFor
+    LightboxModule  // https://www.npmjs.com/package/ngx-lightbox
   ]
 })
 export class GalleryModule { }
