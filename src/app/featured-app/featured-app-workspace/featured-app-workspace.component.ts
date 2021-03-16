7685c994f0e0605d20cc385a04bfa1af4b67cf38
Adam <adam.milko@gmail.com>
2021-03-16 18:53:34 +0100

    Featured App development start

--- src/app/featured-app/featured-app-workspace/featured-app-workspace.component.ts
+++ src/app/featured-app/featured-app-workspace/featured-app-workspace.component.ts
@@ -1,4 +1,6 @@
+import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-featured-app-workspace',
@@ -7,9 +9,34 @@ import { Component, OnInit } from '@angular/core';
 })
 export class FeaturedAppWorkspaceComponent implements OnInit {
 
-  constructor() { }
+  databaseAddr = 'https://amilko-home-default-rtdb.europe-west1.firebasedatabase.app/';
+  // https://amilko-home-default-rtdb.europe-west1.firebasedatabase.app/featuredApp/characters/ermin/.json
+  
+  testFormGroup = new FormGroup({
+    moneyValue: new FormControl(2) // nie chwyta mi wartości z formularza
+  })
+  
+  constructor(
+    private http: HttpClient
+  ) { }
 
   ngOnInit(): void {
+  }
+ 
+  update(){
+    console.log('form val',this.testFormGroup.value.moneyValue)
+    if (!!this.testFormGroup.value.moneyValue){
+      this.http.patch(
+        this.databaseAddr + 'featuredApp/characters/ermin/.json', 
+        {"money" : this.testFormGroup.value.moneyValue}
+      ).subscribe(
+        res => {
+          console.log('received ok response from patch request');
+        }
+      )
+    } else {
+      console.error("!!!!!!!!!!");
+    }
   }
 
 }
--- src/app/featured-app/featured-app.module.ts
+++ src/app/featured-app/featured-app.module.ts
@@ -1,13 +1,20 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FeaturedAppWorkspaceComponent } from './featured-app-workspace/featured-app-workspace.component';
-
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import {MatButtonModule} from '@angular/material/button';
+import { ReactiveFormsModule } from '@angular/forms';
 
 
 @NgModule({
   declarations: [FeaturedAppWorkspaceComponent],
   imports: [
-    CommonModule
+    CommonModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatButtonModule,
+    ReactiveFormsModule
   ]
 })
 export class FeaturedAppModule { }
